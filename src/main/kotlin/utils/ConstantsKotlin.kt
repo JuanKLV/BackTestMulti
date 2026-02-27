b6988764e83c26d1698f19a3fb0e409dd071ba04
leo <gabriel.beltran@onoff.com.co>
2026-02-27 16:21:26 -0500

    puto el que lo lea

--- src/main/kotlin/utils/ConstantsKotlin.kt
+++ src/main/kotlin/utils/ConstantsKotlin.kt
@@ -3,13 +3,13 @@ package utils
 class ConstantsKotlin {
     companion object {
         const val USER_DB = "root"
-        const val PASSWORD = "beltran198"
+        const val PASSWORD = "1234"
         const val USER_DB_SERVER = "leo"
         const val PASSWORD_DB = "tlEAkGMO3YCG9fR7mtET6Pogp7zqNQ5h"
         const val DRIVER = "com.mysql.cj.jdbc.Driver"
         const val DRIVER_POSTGRES = "org.postgresql.Driver"
         const val DRIVER2 = "org.h2.Driver"
-        const val URL_DB_LOCAL = "jdbc:mysql://localhost:3306/Products"
+        const val URL_DB_LOCAL = "jdbc:mysql://172.16.5.64:3306/backtest"
         const val URL_DB_LOCAL2 = "jdbc:h2:file:./build/db"
         const val URL_DB_SERVER = "jdbc:mysql://35.225.4.124:3306/proyectocafe"
         const val URL_DB_SERVER_POSTGRES = "jdbc:postgresql://dpg-csp7mgd6l47c73epra3g-a:5432/proyecto_cafe_eyj0"
